0964b289b41504d5944c5bd829d0bc7c326c2c76
Maria Eduarda Pedroso <mpedroso@alunos.utfpr.edu.br>
2022-09-01 10:47:59 -0300

    Add files via upload

---
+++ DesafioAPI/DesafioAPI/Dados.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Dados
+{
+    public partial class DadosCep
+    {
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string Uf { get; set; }
+        public string Ibge { get; set; }
+        public string Gia { get; set; }
+        public string Ddd { get; set; }
+        public string Siafi { get; set; }
+
+        public void imprimirDados()
+        {
+            Console.WriteLine($"Dados do CEP {this.Cep}");
+            Console.WriteLine($"Logradouro: {this.Logradouro}");
+            Console.WriteLine($"Complemento: {this.Complemento}");
+            Console.WriteLine($"Bairro: {this.Bairro}");
+            Console.WriteLine($"Localidade: {this.Localidade}");
+            Console.WriteLine($"UF: {this.Uf}");
+            Console.WriteLine($"IBGE: {this.Ibge}");
+            Console.WriteLine($"GIA: {this.Gia}");
+            Console.WriteLine($"DDD: {this.Ddd}");
+            Console.WriteLine($"Siafi: {this.Siafi}");
+        }
+    }
+}
+
